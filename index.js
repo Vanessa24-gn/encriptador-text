40a048419e52a6c367fb7d8826656b9cc40cd619
Leidy Gaona <leidy-vanessa-11@hotmail.com>
2024-08-27 05:24:50 -0500

    Añadiendo la funcioalidad de desencriptación de texto

--- index.js
+++ index.js
@@ -26,3 +26,33 @@ function encriptar() {
             alert("Debes ingresar algún texto");
         }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("muneco");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u")
+    .replace(/ola/gi, "l")
+    .replace(/nou/gi, "s")
+    .replace(/mono/gi, "m");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con satisfacción";
+        parrafo.textContent = "";
+        muneco.src = "./img/feliz.png";
+    }
+    else{
+        muneco.src = "./img/Muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
